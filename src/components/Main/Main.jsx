40c01233cc944228ac933fa2ffd4bbf7b8f8efb4
H3nry-T <ht3drex@gmail.com>
2023-02-07 11:47:22 +0000

    Single review added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import styles from "./Main.module.css";
 import ReviewsPage from "./ReviewsPage/ReviewsPage";
+import SingleReview from "./SingleReview/SingleReview";
 const Main = () => {
   return (
     <main className={styles.main}>
       <Routes>
         <Route path="/" element={<ReviewsPage />} />
+        <Route path="/reviews/:review_id" element={<SingleReview />} />
       </Routes>
     </main>
   );
