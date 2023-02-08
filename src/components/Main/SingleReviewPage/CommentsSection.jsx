02c7c2efac0a9c381dc9661542d2c67917e6c3e4
H3nry-T <ht3drex@gmail.com>
2023-02-08 14:15:41 +0000

    comments section can now be viewed

--- src/components/Main/SingleReviewPage/CommentsSection.jsx
+++ src/components/Main/SingleReviewPage/CommentsSection.jsx
@@ -1,42 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import { getCommentsByReviewId } from '../../../utils/axiosSingleReview';
+import React, { useEffect, useState } from "react";
+import { getCommentsByReviewId } from "../../../utils/axiosSingleReview";
 import styles from "./SingleReviewPage.module.css";
 
-
-const CommentsSection = ({review_id}) => {
-  const [commentsByReviewId, setCommentsByReviewId] = useState([]); 
+const CommentsSection = ({ review_id, singleReview }) => {
+  const [commentsByReviewId, setCommentsByReviewId] = useState([]);
   useEffect(() => {
-    getCommentsByReviewId(review_id).then((commentsFromApi) =>{
+    getCommentsByReviewId(review_id).then((commentsFromApi) => {
       console.log(commentsFromApi);
-      setCommentsByReviewId(commentsFromApi); 
-    })
-  }, [])
+      setCommentsByReviewId(commentsFromApi);
+    });
+  }, []);
   return (
     <section className={styles.commentsSection}>
       <div>
-
-      {commentsByReviewId.map((commentObj) => {
-        return (
-        <article key={commentObj.comment_id}>
-          <p>
-            {commentObj.author}'s comment
-          </p>
-          <p>
-            {commentObj.body}
-          </p>
-          <p>
-            {commentObj.created_at}
-          </p>
-          <p className={styles.pSpacing}>
-<button>-</button>
-            <span>{commentObj.votes}</span>
-            <button>+</button>
-          </p>
-        </article>
-      )})}
+        <p className={`${styles.smallWrapper} ${styles.topLeft}`}>
+          {singleReview.comment_count} comments
+        </p>
+        {commentsByReviewId.map((commentObj) => {
+          return (
+            <article key={commentObj.comment_id} className={styles.commentCard}>
+              <p>{commentObj.author}'s comment</p>
+              <p>{commentObj.body}</p>
+              <p>{commentObj.created_at}</p>
+              <div
+                className={`${styles.flexContainer} ${styles.justifyContentCenter}`}
+              >
+                <div className={styles.smallWrapper}>
+                  <button>-</button>
+                  <span>{commentObj.votes}</span>
+                  <button>+</button>
+                </div>
+              </div>
+              <p
+                onClick={(e) => {
+                  console.log(e.target);
+                }}
+              >
+                {commentObj.comment_id}
+              </p>
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default CommentsSection
+export default CommentsSection;
--- src/components/Main/SingleReviewPage/SingleReviewPage.jsx
+++ src/components/Main/SingleReviewPage/SingleReviewPage.jsx
@@ -4,12 +4,11 @@ import { getReviewById } from "../../../utils/axiosSingleReview";
 import CommentsSection from "./CommentsSection";
 import SingleReviewCard from "./SingleReviewCard";
 
-
 const SingleReviewPage = () => {
   const [singleReview, setSingleReview] = useState({});
   const { review_id } = useParams();
   const date = new Date(Date.parse(singleReview.created_at));
-  
+
   useEffect(() => {
     getReviewById(review_id)
       .then((reviewFromApi) => {
@@ -22,8 +21,8 @@ const SingleReviewPage = () => {
 
   return (
     <>
-    <SingleReviewCard singleReview={singleReview} date={date}/>
-    <CommentsSection review_id={review_id}/>
+      <SingleReviewCard singleReview={singleReview} date={date} />
+      <CommentsSection review_id={review_id} singleReview={singleReview} />
     </>
   );
 };
