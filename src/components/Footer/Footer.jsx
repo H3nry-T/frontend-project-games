cc931d245b1fd001e7067792dad09bff49602ac6
H3nry-T <ht3drex@gmail.com>
2023-02-11 22:13:27 +0000

    footer styling and new background color

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return <div className={styles.footer}>Footer</div>;
+  return (
+    <ol className={`${styles.footer}`} style={{ listStyle: "none" }}>
+      <li className={styles.li}>Instagram</li>
+      <li className={styles.li}>LinkedIn</li>
+      <li className={styles.li}>Github</li>
+      <li className={styles.li}>Codepen</li>
+    </ol>
+  );
 };
 
 export default Footer;
