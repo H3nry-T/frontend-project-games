dd9ee26cd2a1a0d1b69f72d138f9a2863e8a7d06
H3nry-T <ht3drex@gmail.com>
2023-02-12 17:47:23 +0000

    profile page is now styled

--- src/components/Main/ProfilesPage/ProfilesPage.jsx
+++ src/components/Main/ProfilesPage/ProfilesPage.jsx
@@ -2,18 +2,46 @@ import { useContext } from "react";
 import { UserContext } from "../../../context/userContext";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styles from "./ProfilesPage.module.css";
 
 const ProfilesPage = () => {
   const navigate = useNavigate();
-  const { setLoggedInUser, userIsLoggedIn, setUserIsLoggedIn, setGlobalError } =
-    useContext(UserContext);
+  const {
+    loggedInUser,
+    setLoggedInUser,
+    userIsLoggedIn,
+    setUserIsLoggedIn,
+    setGlobalError,
+  } = useContext(UserContext);
+  //TODO: MAP OVER MULTIPLE USERS
   return (
     <>
-      <h1>Log in screen</h1>
-      <button onClick={LogUserIn()}>log me in!</button>
-      {userIsLoggedIn ? (
-        <button onClick={signUserOut()}>sign out!</button>
-      ) : null}
+      <section>
+        <h1 style={{ color: "white" }}>Log in:</h1>
+
+        <article className={styles.card}>
+          <h3 className={styles.h3}>weegembump</h3>
+          <img
+            src={
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRayBko6Fh0dBJFOKGKGSDDuH6QeW1RrEKI3-qkTU_Y&s"
+            }
+            className={styles.profileImg}
+          />
+          <div className={styles.container}>
+            <button className={styles.profilePageButtons} onClick={LogUserIn()}>
+              log me in!
+            </button>
+            {userIsLoggedIn ? (
+              <button
+                className={styles.profilePageButtons}
+                onClick={signUserOut()}
+              >
+                sign out!
+              </button>
+            ) : null}
+          </div>
+        </article>
+      </section>
     </>
   );
 
