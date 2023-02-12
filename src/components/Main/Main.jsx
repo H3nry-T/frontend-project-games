c313c48d1632f75e567df3916062ba89bab139e9
H3nry-T <ht3drex@gmail.com>
2023-02-12 00:51:40 +0000

    default path login screen

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import styles from "./Main.module.css";
 import ReviewsPage from "./ReviewsPage/ReviewsPage";
 import SingleReviewPage from "./SingleReviewPage/SingleReviewPage";
 const Main = () => {
+  const navigate = useNavigate();
   return (
     <main className={styles.main}>
       <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h1>Log in screen</h1>
+              <button
+                onClick={() => {
+                  navigate("/reviews");
+                }}
+              >
+                log me in!
+              </button>
+            </>
+          }
+        />
         <Route path="/*" element={<ReviewsPage />} />
         <Route path="/reviews/:review_id" element={<SingleReviewPage />} />
       </Routes>
